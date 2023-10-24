61f0bf4030eb7b078fe520aee2ded356b2e84bb7
Vitor Duarte <duartevitor178@gmail.com>
2023-10-24 16:42:46 +0200

    refactor | fixed error and added bonus to dice

--- dice-throw.js
+++ dice-throw.js
@@ -1,20 +1,25 @@
+let userWin = 0;
+let botWin = 0;
+
 do {
     let random = Math.random() * 6;
     let random2 = Math.random() * 6;
     let diceThrowUser = Math.floor(random) + 1;
-    let diceThrowBot = Math.floor(random2) + 1;
+    let diceThrowBot = Math.floor(random2) + 1
     
     alert("Your result is " + diceThrowUser + " the opponent's result is " + diceThrowBot);
     
     if(diceThrowUser > diceThrowBot) {
         alert("You won")
-        userWin = userWin ++;
+        userWin ++;
     } else if(diceThrowUser === diceThrowBot) {
-        alert("You tie")
-        botWin = botWin ++;
+        alert("You tie");
     } else {
         alert("You lose")
+        botWin ++;
     }
+
+    alert(userWin + " " + botWin)
 
     let nextRoundReply = prompt('Play next round?');
     if (nextRoundReply === 'No') {
--- guessing-game.js
+++ guessing-game.js
@@ -1,17 +1,16 @@
-let userChoice = Number(prompt("Select the highest number to guess"))
-
+let userChoice = Number(prompt("Select the highest number to guess"));
 
 let random = Math.random() * userChoice;
 let randomNumber = Math.floor(random) + 1;
-let userGuess = prompt("Guess a number from 1-" + userChoice)
+let userGuess = prompt("Guess a number from 1-" + userChoice);
 
 console.log(randomNumber)
 
 if(userGuess > randomNumber) {
-    alert("Your number is too high")
+    alert("Your number is too high");
 } else if (userGuess < randomNumber){
-    alert("Your number is too low")
+    alert("Your number is too low");
 } else if (userGuess === randomNumber){
-    alert("You guessed it")
+    alert("You guessed it");
 }
 
